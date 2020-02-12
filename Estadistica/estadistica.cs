cdab1c18a5b48b6222d6e26025f53ec4714e75ac
Maverick0612 <maverick.penado@gmail.com>
2020-02-12 13:34:36 -0600

    creacion de la clase

---
+++ Estadistica/estadistica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estadistica
+{
+    class estadistica
+    {
+        //creamos el metodo una matriz
+        public double  suma = 0;
+        public double media(string[] serie)
+        {
+            //recorrer 
+            foreach (string valor in serie)
+            {
+                suma += int.Parse(valor);
+            }
+            return suma / serie.Length;
+        }      
+    }
+}
